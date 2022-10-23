a50cdda23aa18135083955a44395b5e8f5878fdb
tinkerboy <gio@tinkerboy.xyz>
2022-10-23 13:42:54 +0800

    Updated and moved config.h to 16u2 folder.

--- keyboards/converter/tinkerboy/adb_usb/config.h
+++ keyboards/converter/tinkerboy/adb_usb/config.h
@@ -18,37 +18,6 @@ Ported to QMK by Peter Roe <pete@13bit.me>
 */
 
 #pragma once
-/*
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0ADB
-#define DEVICE_VER      0x0102
-#define MANUFACTURER    TINKERBOY
-#define PRODUCT         tinkerBOY [adb.usb]
-#define DESCRIPTION     adb.to.usb
-#define VIAL_TAP_DANCE_ENTRIES 0
-#define VIAL_COMBO_ENTRIES 0
-#define VIAL_KEY_OVERRIDE_ENTRIES 0
-*/
-#define DYNAMIC_KEYMAP_LAYER_COUNT 1
-#define NO_ACTION_LAYER
-
-#define BOOTMAGIC_LITE_ROW 1
-#define BOOTMAGIC_LITE_COLUMN 3
-
-#ifndef NO_DEBUG
-#define NO_DEBUG
-#endif // !NO_DEBUG
-#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
-#define NO_PRINT
-#endif // !NO_PRINT
-
-#define NO_ACTION_ONESHOT
-#define NO_MUSIC_MODE
-#undef LOCKING_SUPPORT_ENABLE
-#undef LOCKING_RESYNC_ENABLE
-#define LAYER_STATE_8BIT
-
-#define NO_ACTION_TAPPING
 
 /* matrix size */
 #define MATRIX_ROWS 16  // keycode bit: 3-0
